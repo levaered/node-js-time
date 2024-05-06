9239057799be5e41a91ceb4fad05c3b9c6d102f3
levaered <61624689+levaered@users.noreply.github.com>
2024-05-06 13:24:52 +0300

    added satus Code, and local time

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-import express from 'express'
+import express from 'express';
 
 const port = 3333;
 const app = express();
 
+const getCurrentTime = () => {
+    const now = new Date();
+    const localTime = now.toLocaleTimeString(); 
+    return localTime;
+};
+
 app.get('/get-time', (req, res) => {
-    const currentDate = {
-        currentDate: new Date()
-    }
-    res.json(currentDate);
-})
+    const currentTime = {
+        currentTime: getCurrentTime()
+    };
+    res.status(200).json(currentTime);
+});
 
 app.listen(port, () => {
-    console.log(`Server listining on port ${port}`)
-})
+    console.log(`Server listening on port ${port}`);
+});
